3f3a79669179239302c19c040699120a3dbcd8f3
vossenwout <vossenwout@users.noreply.github.com>
2025-11-10 19:02:53 +0000

    puff v0.3.0

--- Formula/puff.rb
+++ Formula/puff.rb
@@ -1,9 +1,9 @@
 class Puff < Formula
   desc "CLI helper that fires macOS notifications for AI agents"
   homepage "https://github.com/vossenwout/puff"
-  url "https://github.com/vossenwout/puff/releases/download/v0.2.2/puff-v0.2.2.tar.gz"
-  sha256 "c9dc29dbce74b661fafd27260a26bb48b851deef0d2b6ef99183596a49dc53e0"
-  version "0.2.2"
+  url "https://github.com/vossenwout/puff/releases/download/v0.3.0/puff-v0.3.0.tar.gz"
+  sha256 "444ce966b8fb5c70230789cd442a778d2cb2800bbb394b1d2e294397a9ad0591"
+  version "0.3.0"
   license "MIT"
 
   def install
